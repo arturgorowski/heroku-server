32be5f1fa4502a5f863bee70eef05112f70b2d3d
Artur Górowski <artek159@gmail.com>
2019-03-25 23:04:40 +0100

    heroku server

---
+++ app/app.js
@@ -0,0 +1,23 @@
+'use strict';
+
+import express from 'express';
+import bodyParser from "body-parser";
+
+const { Client } = require('pg');
+//const bodyParser = require('body-parser');
+const app = express();
+
+app.use(bodyParser.json());
+
+/*const pool = new Pool({
+  connectionString: process.env.DATABASE_URL
+});*/
+
+app.get('/', (req, res)=> {
+  res.render('cos')
+})
+
+
+app.listen(process.env.PORT, function(){
+  console.log("Server is running");
+})
