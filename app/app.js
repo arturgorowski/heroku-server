cfebf1f29465808ffb4cc55f14a017568c83fcf0
Artur Górowski <artek159@gmail.com>
2019-03-27 22:44:27 +0100

    api/devices

--- app/app.js
+++ app/app.js
@@ -1,9 +1,26 @@
 'use strict';
-
-
 import express from 'express';
 import bodyParser from "body-parser";
-import Client from 'pg';
+//import Client from 'pg';
+
+
+const devices = [
+  {id_device: 1,name:'light'},
+  {id_device: 2,name:'blinds'},
+  {id_device: 3,name:'air_conditioning'},
+  {id_device: 4,name:'heating'},
+  {id_device: 5,name:'sound'},
+  {id_device: 6,name:'alarm'},
+  {id_device: 7,name:'cameras'},
+  {id_device: 8,name:'front_door'},
+  {id_device: 9,name:'garage_door'},
+  {id_device: 10,name:'gate'},
+  {id_device: 11,name:'weather_station'},
+  {id_device: 12,name:'fridge'},
+  {id_device: 13,name:'TV'},
+  {id_device: 14,name:'washer'},
+  {id_device: 15,name:'oven'}
+];
 
 const app = express();
 app.use(bodyParser.json());
@@ -17,13 +34,18 @@ app.get('/', (req, res)=> {
   res.json('KURWA JA PIERDOLE JEBANY SUKCES!!')
 })
 
-app.get('/api/device', (req, res)=>{
-  pool.query('SELECT * FROM device', (error, results)=>{
+app.get('/api/device/:id', (req, res)=>{
+  /*pool.query('SELECT * FROM device', (error, results)=>{
     if(error){
       throw error
     }
     res.json(res.rows)
-  })
+  })*/
+  const post = posts.find((p) => p.id === parseInt(req.params.id));
+  if (!post) {
+    res.status(404).send("Post NotFound");
+  }
+  res.send(post);
 })
 
 
