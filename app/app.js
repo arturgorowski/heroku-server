076e9291116f0afd701d5564746ebcd28352798a
Artur Górowski <artek159@gmail.com>
2019-03-28 22:40:07 +0100

    test

--- app/app.js
+++ app/app.js
@@ -2,29 +2,29 @@
 
 import express from 'express';
 import bodyParser from "body-parser";
-import Client from 'pg';
+//import Client from 'pg';
 
 
 const app = express();
 app.use(bodyParser.json());
 
 //const Pool = require('pg').Pool
-const pool = new Client({
+/*const pool = new Client({
   connectionString: process.env.DATABASE_URL
-})
+})*/
 
 app.get('/', (req, res)=> {
   res.json('KURWA JA PIERDOLE JEBANY SUKCES!!')
 })
 
-app.get('/api/device', (req, res)=>{
+/*app.get('/api/device', (req, res)=>{
   pool.query('SELECT * FROM device', (error, results)=>{
     if(error){
       throw error
     }
     res.status(200).json(res.rows)
   })
-})
+})*/
 
 
 app.listen(process.env.PORT, function(){
