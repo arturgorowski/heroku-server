c9394932fedf1bfcf5882d7402fc7817e91c143a
krystianlisowski <krystianlisowski96@gmail.com>
2019-03-29 10:39:00 +0100

    post

--- app/app.js
+++ app/app.js
@@ -46,7 +46,29 @@ const device = {
       } catch(error) {
         return res.status(400).send(error);
       }
-    }
+    },
+
+    async create(req, res) {
+      const text = `INSERT INTO
+        device(id_device, name)
+        VALUES($1, $2)
+        returning *`;
+      const values = [
+        uuidv4(),
+        req.body.success,
+        req.body.low_point,
+        req.body.take_away,
+        moment(new Date()),
+        moment(new Date())
+      ];
+  
+      try {
+        const { rows } = await db.query(text, values);
+        return res.status(201).send(rows[0]);
+      } catch(error) {
+        return res.status(400).send(error);
+      }
+    },
   }
 
 app.get('/', (req, res)=> {
@@ -54,8 +76,9 @@ app.get('/', (req, res)=> {
 })
 
 app.get('/api/devices', device.getAll);
-
+app.post('/api/devices', device.create);
 
 app.listen(process.env.PORT, function(){
   console.log("Server is running");
 })
+
