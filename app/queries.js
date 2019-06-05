d0031b1188f2807b805dca120b0df9df6078767c
Artur Górowski <artek159@gmail.com>
2019-06-05 19:43:34 +0200

    get user device

--- app/queries.js
+++ app/queries.js
@@ -39,7 +39,7 @@ const createUser = (request, response) => {
 const userId = (request, response) => {
     const { email } = request.params
 
-    pool.query('SELECT id_user, first_name, last_name, email FROM public.users WHERE email=$1', [email], (error, results) => {
+    pool.query('SELECT id_user FROM public.users WHERE email=$1', [email], (error, results) => {
         if (error) {
             throw error
         }
